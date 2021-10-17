c3271092c45f581b9fd60d70e6612c9f3b7d0cc6
ikuno9233 <ikuno9233@gmail.com>
2021-10-17 20:29:08 +0900

    [edit] 機能項目3 災害Eventページへのリンクを追加

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -1,43 +1,29 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.base')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>災害情報ポータルサイト</title>
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-</head>
+@section('title', '災害情報ポータルサイト')
 
-<body id="event-index" class="pb-2">
-    <header class="navbar navbar-light bg-light fixed-top shadow cust-header">
-        <div class="container-fluid">
-            <span class="navbar-brand mx-0 text-center text-light">災害情報ポータルサイト</span>
-            <span></span>
-            <span></span>
-        </div>
-    </header>
-    <main class="container mt-lg-5 mt-3">
-        <div class="row justify-content-center">
-            <div class="col-lg-8 col-md-10">
-                <div class="row row-cols-1 row-cols-md-2">
-                    @forelse ($emergencyEvents as $emergencyEvent)
-                        <div class="col p-1">
-                            <div class="card shadow-sm">
-                                <div class="card-body">
-                                    <h5 class="card-title text-secondary">{{ $emergencyEvent->event_title }}</h5>
-                                </div>
-                            </div>
+@section('body_id', 'event-index')
+
+@section('navbar')
+    <span class="navbar-brand mx-0 text-center text-light">災害情報ポータルサイト</span>
+    <span></span>
+    <span></span>
+@endsection
+
+@section('contents')
+    <div class="row row-cols-1 row-cols-md-2">
+        @forelse ($emergencyEvents as $emergencyEvent)
+            <div class="col p-1">
+                <a href="{{ route('event.show', ['id' => $emergencyEvent->ee_id]) }}">
+                    <div class="card shadow-sm">
+                        <div class="card-body">
+                            <h5 class="card-title text-secondary">{{ $emergencyEvent->event_title }}</h5>
                         </div>
-                    @empty
-                        <span>Not found.</span>
-                    @endforelse
-                </div>
+                    </div>
+                </a>
             </div>
-        </div>
-    </main>
-
-    <script src="{{ mix('/js/app.js') }}"></script>
-</body>
-
-</html>
+        @empty
+            <span>Not found.</span>
+        @endforelse
+    </div>
+@endsection
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+</head>
+
+<body id="@yield('body_id')" class="pb-2">
+    <header class="navbar navbar-light bg-light fixed-top shadow cust-header">
+        <div class="container-fluid">
+            @yield('navbar')
+        </div>
+    </header>
+    <main class="container mt-lg-5 mt-3">
+        <div class="row justify-content-center">
+            <div class="col-lg-8 col-md-10">
+                @yield('contents')
+            </div>
+        </div>
+    </main>
+
+    <script src="{{ mix('/js/app.js') }}"></script>
+</body>
+
+</html>
