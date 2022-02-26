78e44f134fc19ddc92e2e5dc63e61dec20915424
kobajun0219 <kobajunoasis@yahoo.co.jp>
2022-02-26 11:57:03 +0900

    fix app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,7 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Menu links -->
+                        @Auth
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
@@ -54,6 +55,7 @@
                                 </form>
                             </div>
                         </li>
+                        @endif
                     </ul>
                 </div>
             </div>
